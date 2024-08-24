c6146584944261e450ff076a0dd64c44fc82b958
Dattahari Kadam <dattahrik61@gmail.com>
2024-08-24 19:23:40 +0530

    adding existing project

---
+++ src/components/Logo.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Logo({width, height}) {
+  return (
+    <img width={width} height={height} src="./blog.png" alt="tech-blog logo" />
+  )
+}
+
+export default Logo
